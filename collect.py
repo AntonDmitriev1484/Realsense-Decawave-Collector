98961346137de5d93b814c6e60f4dfba3735b24f
AntonDmitriev1484 <a.dmitriev@wustl.edu>
2025-05-06 11:11:35 -0500

    Fixed collect process hanging, added clock drift check

--- collect.py
+++ collect.py
@@ -3,7 +3,7 @@ import sys
 import os
 import argparse
 import threading
-from multiprocessing import Queue, Process
+from multiprocessing import Queue, Process, Event
 import serial
 import cv2
 import numpy as np
@@ -20,12 +20,6 @@ pyd_path = "C:\\Program Files\\Intel RealSense SDK 2.0\\bin\\x64\\"
 sys.path.append(pyd_path)
 import pyrealsense2 as rs
 
-# import psutil
-# import os
-
-# p = psutil.Process(os.getpid())
-# p.nice(psutil.HIGH_PRIORITY_CLASS)  # Sets to High priority
-
 # Package is for 3.11
 
 # py -3.11 -m venv py311
@@ -40,14 +34,12 @@ if len(sys.argv) < 2:
 
 args = parser.parse_args()
 
-DATA_DIR = f"./{args.trial_name}_raw/"
+DATA_DIR = f"./data/{args.trial_name}_raw/"
 RGB_DIR = DATA_DIR + "rgb/"
 DEPTH_DIR = DATA_DIR + "depth/"
 ACCEL_FILE = DATA_DIR + "accel.json"
 GYRO_FILE = DATA_DIR + "gyro.json"
 UWB_FILE = DATA_DIR + "uwb.json"
-RGB_FILE = DATA_DIR + "rgb.json"
-DEPTH_FILE = DATA_DIR + "depth.json"
 
 try: os.makedirs(DATA_DIR, exist_ok=True)
 except OSError as e: print(e)
@@ -58,23 +50,12 @@ except OSError as e: print(e)
 try: os.makedirs(DEPTH_DIR, exist_ok=True)
 except OSError as e: print(e)
 
-
-# To check what the possible streaming rates are
-def check_sensors():
-    sensors = device.query_sensors()
-    for sensor in sensors:
-        print(f"Sensor: {sensor.get_info(rs.camera_info.name)}")
-        for p in sensor.get_hosttream_profiles():
-            print(f"  {p.stream_type()}, {p.format()}, {p.fps()} fps")
-
 def host_timestamp(): return time.perf_counter_ns() / 1e6 # Return host time in ms.
 
 
-stop_signal = threading.Event()
-stop_complete_signal = threading.Event()
 
-def realsense_listener():
-    global accel, gyro, rgb, depth
+def realsense_listener(stop_signal, stop_complete_signal):
+    # global stop_signal, stop_complete_signal
 
     pipeline = rs.pipeline()
     config = rs.config()
@@ -92,8 +73,10 @@ def realsense_listener():
     rgb = Queue(maxsize=10)
     depth = Queue(maxsize=10)
 
-    ACCEL_FS = open(ACCEL_FILE, 'w')
-    GYRO_FS = open(GYRO_FILE, 'w')
+    ACCEL_HANDLE = open(ACCEL_FILE, 'w', newline='')
+    ACCEL_FS = csv.writer(ACCEL_HANDLE)
+    GYRO_HANDLE = open(GYRO_FILE, 'w', newline='')
+    GYRO_FS = csv.writer(GYRO_HANDLE)
 
     # For color sensor
     color_profiles = rgb_camera.get_stream_profiles()
@@ -144,25 +127,31 @@ def realsense_listener():
         rgb.put({"t_dev": frame.get_timestamp(), "t_host": host_timestamp(), "data":np.asanyarray(frame.get_data())})
 
     def depth_camera_callback(frame):
+        # print("Depth callback") # Depth just runs at very sparse (sub 30fps intervals), a bursrt at the beginning, and then very low frequency
         depth.put({"t_dev": frame.get_timestamp(), "t_host": host_timestamp(), "data":np.asanyarray(frame.get_data())})
 
     def flush_buffers():
         if accel.full():
             print("Flushing accel")
-            while not accel.empty(): json.dump(accel.get(block=False), ACCEL_FS, indent=1)
+            while not accel.empty():
+                x = accel.get(block=False)
+                ACCEL_FS.writerow([x['t_dev'], x['t_host'], x['data'][0], x['data'][1], x['data'][2]])
+                # json.dump(accel.get(block=False), ACCEL_FS, indent=1)
         if gyro.full(): 
             print("Flushing gyro")
-            while not gyro.empty(): json.dump(gyro.get(block=False), GYRO_FS, indent=1)
+            while not gyro.empty():
+                x = gyro.get(block=False)
+                GYRO_FS.writerow([x['t_dev'], x['t_host'], x['data'][0], x['data'][1], x['data'][2]])
         if rgb.full():
             print("Flushing RGB")
             while not rgb.empty():
                 frame = rgb.get()
-                np.savez(RGB_DIR+"/"+str(frame["t_host"]/1e3)+".npz", array= frame["data"], meta= f"{ [kv for kv in frame.items() if not kv[0] == 'data'] }" )
+                np.savez( RGB_DIR+"/"+str(frame["t_host"]/1e3)+".npz", array= frame["data"], t_dev=frame["t_dev"], t_host = frame["t_host"] )
         if depth.full():
             print("Flushing depth")
             while not depth.empty():
                 frame = depth.get()
-                np.savez(DEPTH_DIR+"/"+str(frame["t_host"]/1e3)+".npz", array= frame["data"], meta= f"{[kv for kv in frame.items() if not kv[0] == 'data']}")
+                np.savez( DEPTH_DIR+"/"+str(frame["t_host"]/1e3)+".npz", array= frame["data"], t_dev=frame["t_dev"], t_host = frame["t_host"] )
     
     print(f"Realsense set up callbacks ")
     imu.start(imu_callback)
@@ -170,7 +159,9 @@ def realsense_listener():
     depth_camera.start(depth_camera_callback)
     print(f"Realsense started collection")
 
-    while not stop_signal.is_set(): flush_buffers()
+    while not stop_signal.is_set():
+        # print(stop_signal)
+        flush_buffers()
     flush_buffers()
 
     print("Escaped loop")
@@ -179,8 +170,8 @@ def realsense_listener():
     rgb_camera.stop()
     depth_camera.stop()
     print("Stopped realsense")
-    ACCEL_FS.close()
-    GYRO_FS.close()
+    ACCEL_HANDLE.close()
+    GYRO_HANDLE.close()
     print("Realsense done closing")
 
     stop_complete_signal.set()
@@ -196,67 +187,90 @@ def write_to_serial(ser, str):
 def fetch_decawave_time(ser): write_to_serial(ser, "AT+TIME") # Issue command to get hardware timestamp
 
 
-uwb = []
-deca_time = []
-def decawave_listener(): # Can use the timestamp I log in decawave serial as that hardware time
-    
-    TAG_PORT = 'COM4'
-    TAG_SERIAL = serial.Serial(TAG_PORT, 115200)
+# def on_interrupt(sig, frame):
+#     print("Interrupt")
+#     stop_signal.set()
+#     stop_complete_signal.wait()
+#     realsense_process.join()
+#     print("Exited realsense")
+#     with open(UWB_FILE, 'w') as fs: json.dump(uwb, fs, indent=1) # Dump UWB data
 
-    ht_query_limit = 5 # Very laggy with a query limit of 1
-    range_counter = 0
-
-    fetch_decawave_time(TAG_SERIAL)
-    print(f"Decawave started collection")
-
-    while True:
-
-        line = read_from_serial(TAG_SERIAL)
-        if line is not None:
-            if "{" in line and "}" in line: 
-                # print(line)
-                if len(deca_time) > 0:
-                    estimated_t_dev = (host_timestamp() - deca_time[-1]["t_host"]) + deca_time[-1]["t_dev"]
-                    uwb.append({"t_dev": estimated_t_dev, "t_host": host_timestamp(), "data":json.loads(line)})
-                    range_counter += 1
-            elif "Time:" in line:
-                # deca_time.append({"t_host": time.perf_counter(), "t_dev": int(line.split()[1]) })
-                match = re.search(r'\d+', line)
-                if match:
-                    deca_time.append({"t_dev": int(match.group()), "t_host": host_timestamp()})
-
-        if range_counter > ht_query_limit:  
-            fetch_decawave_time(TAG_SERIAL)
-            range_counter = 0
-
-
-def on_interrupt(sig, frame):
-    print("Interrupt")
-    stop_signal.set()
-    stop_complete_signal.wait()
-    realsense_process.join()
-    # realsense_process.kill()
-    print("Exited realsense")
-    with open(UWB_FILE, 'w') as fs: json.dump(uwb, fs, indent=1) # Dump UWB data
-
-    sys.exit(0)
+#     sys.exit(0)
 
 if __name__ == "__main__":
 
-    signal.signal(signal.SIGINT, on_interrupt)
+    # signal.signal(signal.SIGINT, on_interrupt)
 
     print("Starting Realsense thread") 
     # realsense_listener() # Library implicitly spawns off 3 threads
 
-    realsense_process = Process(target=realsense_listener)
+    stop_signal = Event()
+    stop_complete_signal = Event()
+
+    realsense_process = Process(target=realsense_listener, args = (stop_signal, stop_complete_signal))
     realsense_process.start()
 
+    uwb = []
+    deca_time = []
+    def decawave_listener(): # Can use the timestamp I log in decawave serial as that hardware time
+        
+        TAG_PORT = 'COM4'
+        TAG_SERIAL = serial.Serial(TAG_PORT, 115200)
+
+        ht_query_limit = 5 # Very laggy with a query limit of 1
+        range_counter = 0
+
+        fetch_decawave_time(TAG_SERIAL)
+        print(f"Decawave started collection")
+
+        while True:
+            line = read_from_serial(TAG_SERIAL)
+            if line is not None:
+                if "{" in line and "}" in line: 
+                    # print(line)
+                    if len(deca_time) > 0:
+                        estimated_t_dev = (host_timestamp() - deca_time[-1]["t_host"]) + deca_time[-1]["t_dev"]
+                        uwb.append({"t_dev": estimated_t_dev, "t_host": host_timestamp(), "data":json.loads(line)})
+                        range_counter += 1
+                elif "Time:" in line:
+                    # deca_time.append({"t_host": time.perf_counter(), "t_dev": int(line.split()[1]) })
+                    match = re.search(r'\d+', line)
+                    if match:
+                        deca_time.append({"t_dev": int(match.group()), "t_host": host_timestamp()})
+
+            if range_counter > ht_query_limit:  
+                fetch_decawave_time(TAG_SERIAL)
+                range_counter = 0
+
+            if time.perf_counter() - START_T > 120: break
+        return
+
+    START_T = time.perf_counter()
     print("Starting Decawave thread")
     decawave_listener() # Can run on main thread
 
-    
-    # TODO: They only allow you to have 16 frames in memory at a given time, which is why my frames cap out at 16
-    # So I would have to flush frames out of memory asynchronously for the realsense
-    # To do this I need the process API, which is a mess of its own because its a full process clone.
+    print("Interrupt")
+    stop_signal.set()
+    print("Between stop set and wait complete")
+    stop_complete_signal.wait()
+    print("stop completed, passing on")
+    # realsense_process.join() # Don't join an exited process
+    print("Exited realsense")
+
+    with open(UWB_FILE, 'w') as fs: json.dump(uwb, fs, indent=1) # Dump UWB data
+    print("UWB dump complete")
+
+    # Force kill if process still alive
+    if realsense_process.is_alive():
+        print("Force terminating realsense process")
+        realsense_process.terminate()
+        realsense_process.join()
+    else:
+        print("Realsense process exited cleanly")
+        # Never run this as process hangs on exiting realsense
+            
+        # TODO: They only allow you to have 16 frames in memory at a given time, which is why my frames cap out at 16
+        # So I would have to flush frames out of memory asynchronously for the realsense
+        # To do this I need the process API, which is a mess of its own because its a full process clone.
 
 
--- post_process.py
+++ post_process.py
@@ -1,8 +1,74 @@
 
 
+import sys
+import os
+import argparse
+import threading
+from multiprocessing import Queue, Process, Event
+import serial
+import cv2
+import numpy as np
+import time
+import signal
+import csv
+import json
+import re
+
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+pyd_path = "C:\\Program Files\\Intel RealSense SDK 2.0\\bin\\x64\\"
+sys.path.append(pyd_path)
+import pyrealsense2 as rs
+
+# Package is for 3.11
+
+# py -3.11 -m venv py311
+# .\py311\Scripts\Activate
+
+parser = argparse.ArgumentParser(description="Stream collector")
+parser.add_argument("--trial_name" , "-t", type=str)
+
+if len(sys.argv) < 2:
+    print("Provide -t <trial_name>")
+    exit()
+
+args = parser.parse_args()
+RAW_DATA_DIR = f"./data/{args.trial_name}_raw/"
+RGB_DIR = RAW_DATA_DIR + "rgb/"
+DEPTH_DIR = RAW_DATA_DIR + "depth/"
+ACCEL_FILE = RAW_DATA_DIR + "accel.json"
+GYRO_FILE = RAW_DATA_DIR + "gyro.json"
+UWB_FILE = RAW_DATA_DIR + "uwb.json"
+
+accel, gyro, rgb, uwb = ([], [], [], [])
+
+# for depth_file in os.listdir(DEPTH_DIR):
+#     data = np.load(DEPTH_DIR+depth_file)
+#     depth.append({key: data[key].item() if data[key].ndim == 0 else data[key] for key in data.files})
+
+for rgb_file in os.listdir(RGB_DIR):
+    data = np.load(RGB_DIR+rgb_file)
+    rgb.append({key: data[key].item() if data[key].ndim == 0 else data[key] for key in data.files})
+
+with open(ACCEL_FILE, 'r') as fs: 
+    csvreader = csv.reader(fs)
+    for row in csvreader:
+        accel.append({"t_dev": float(row[0]), "t_host": float(row[1]), "data":(float(row[2]), float(row[3]), float(row[4]))})
+
+with open(GYRO_FILE, 'r') as fs: 
+    csvreader = csv.reader(fs)
+    for row in csvreader:
+        gyro.append({"t_dev": float(row[0]), "t_host": float(row[1]), "data":(float(row[2]), float(row[3]), float(row[4]))})
+
+uwb = json.load(open(UWB_FILE, 'r'))
+
+
 def postprocess_data():
 
-    sensor_streams = [accel, gyro, rgb, depth, uwb]
+    sensor_streams = [accel, gyro, rgb,  uwb]
+
+    # print(accel[0])
     HOST_START = max([ s[0]["t_host"] for s in sensor_streams])
     print(f" {HOST_START=}")
 
@@ -16,28 +82,33 @@ def postprocess_data():
 
     print(f" Starting host timestamps {[ s[0]['t_host'] for s in sensor_streams]}")
 
-    # # for sensordata in [accel, gyro, rgb, depth]:
-    # #     for a in sensordata: 
-    # #         a["t_host"] -= HOST_START
-    # #         a["t_dev"] -= REALSENSE_START
-
     DECAWAVE_START = uwb[0]["t_dev"]
-    REALSENSE_START = min(accel[0]["t_dev"], gyro[0]["t_dev"], rgb[0]["t_dev"], depth[0]["t_dev"])
+    REALSENSE_START = min(accel[0]["t_dev"], gyro[0]["t_dev"], rgb[0]["t_dev"])
+
+    for sensordata in [accel, gyro, rgb]:
+        for a in sensordata: 
+            a["t_host"] -= HOST_START
+            a["t_dev"] -= REALSENSE_START
+
+    for sensordata in [uwb]:
+        for a in sensordata: 
+            a["t_host"] -= HOST_START
+            a["t_dev"] -= DECAWAVE_START
 
     # ITS like the for loop just doesn't iterate over the full list?
     # Could it be that the realsense thread is still modifying the list after I told it to stop? No...
     # I have absolutely no clue what the fuck is wrong with this program.
 
-    for i in range(0,4): # Realsense
-        for j in range(0, len(sensor_streams[i])):
-            sensor_streams[i][j]["t_dev"] -= REALSENSE_START
-            sensor_streams[i][j]["t_host"] -= HOST_START
+    # for i in range(0,3): # Realsense
+    #     for j in range(0, len(sensor_streams[i])):
+    #         sensor_streams[i][j]["t_dev"] -= REALSENSE_START
+    #         sensor_streams[i][j]["t_host"] -= HOST_START
     
-    i=0
-    for i in range(4,5): # UWB
-        for j in range(len(sensor_streams[i])):
-            sensor_streams[i][j]["t_dev"] -= DECAWAVE_START
-            sensor_streams[i][j]["t_host"] -= HOST_START
+    # i=0
+    # for i in range(3,4): # UWB
+    #     for j in range(len(sensor_streams[i])):
+    #         sensor_streams[i][j]["t_dev"] -= DECAWAVE_START
+    #         sensor_streams[i][j]["t_host"] -= HOST_START
 
 
     decawave_ts = []
@@ -60,19 +131,27 @@ def postprocess_data():
 
     decawave_ts = np.array(decawave_ts)
     accel_ts = np.array(accel_ts)
-    # print(accel_ts)
+    print(accel_ts)
     # print(accel_ts.shape)
     realsense_ts = np.array(realsense_ts)
 
     plt.title(" Hardware (Decawave, Realsense IMU, RGB) Timestamps vs Host Timestamps")
-    plt.scatter( accel_ts[:,0], accel_ts[:,1], label='Realsense IMU')
-    plt.scatter( realsense_ts[:,0], realsense_ts[:,1], label='Realsense RGB')
-    plt.scatter( decawave_ts[:,0], decawave_ts[:,1], label='Decawave')
+    plt.plot( accel_ts[:,0], accel_ts[:,1], label='Realsense IMU')
+    plt.plot( realsense_ts[:,0], realsense_ts[:,1], label='Realsense RGB')
+    plt.plot( decawave_ts[:,0], decawave_ts[:,1], label='Decawave')
     # plt.scatter( accel_ts, label='Realsense IMU')
     # plt.scatter( realsense_ts, label='Realsense RGB')
     # plt.scatter( decawave_ts, label='Decawave')
     plt.legend()
     plt.show()
+
+    print(" Clock Drift Slope: ")
+    m, b = np.polyfit(accel_ts[:,0], accel_ts[:,1], 1)
+    print(f" Accel = {m}")
+    m, b = np.polyfit(realsense_ts[:,0], realsense_ts[:,1], 1)
+    print(f" RGB = {m}")
+    m, b = np.polyfit(decawave_ts[:,0], decawave_ts[:,1], 1)
+    print(f" UWB = {m}")
 
     # for a in accel:
     #     at = accel["t_host"]
@@ -102,3 +181,5 @@ def postprocess_data():
     # for a in accel:
     #     print()
     #     print(f"{a=}")
+
+postprocess_data()
